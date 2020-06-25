d3d58474054eaea36af40c01c1763092e50a7cf6
woosup <fbdntjq2@gmail.com>
2020-06-25 10:44:40 +0900

    2020/06/25

--- mqtt.py
+++ mqtt.py
@@ -12,23 +12,23 @@ class mqtt_callback():
         else:
             print("Bad connection Returned code=", rc)
 
-def on_disconnect(self, client, userdata, flags, rc=0):
-    print(str(rc))
+    def on_disconnect(self, client, userdata, flags, rc=0):
+        print(str(rc))
 
-def on_publish(self, client, userdata, mid):
-    print("In on_pub callback mid= ", mid)
+    def on_publish(self, client, userdata, mid):
+        print("In on_pub callback mid= ", mid)
 
-def on_subscribe(self, client, userdata, mid, granted_qos):
-    print("subscribed: " + str(mid) + " " + str(granted_qos))
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        print("subscribed: " + str(mid) + " " + str(granted_qos))
 
-def on_message(self, client, userdata, message):
-    #         print("message received " ,str(message.payload.decode("utf-8")))
-    #         print("message topic=",message.topic)<br>#         print("message qos=",message.qos)<br>#         print("message retain flag=",message.retain)<br>#         print("userdata=", userdata)<br>        print('class method device_id : ', self.device_id)<br>        if message.topic == "device_operation_vending_web":
-    data = json.loads(str(message.payload.decode("utf-8")))
-    if data['ret_code'] == "0000":
-        client.disconnect()
-    else:
-        print('fail!')
+    def on_message(self, client, userdata, message):
+        #         print("message received " ,str(message.payload.decode("utf-8")))
+        #         print("message topic=",message.topic)<br>#         print("message qos=",message.qos)<br>#         print("message retain flag=",message.retain)<br>#         print("userdata=", userdata)<br>        print('class method device_id : ', self.device_id)<br>        if message.topic == "device_operation_vending_web":
+        data = json.loads(str(message.payload.decode("utf-8")))
+        if data['ret_code'] == "0000":
+            client.disconnect()
+        else:
+            print('fail!')
 
 
 class mqtt_connector(mqtt_callback):
